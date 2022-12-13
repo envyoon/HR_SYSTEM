5538a272c2d70150483cec36c62df7340a82b9a0
YoonPro <krafftdj@daum.net>
2022-12-13 23:10:55 +0900

    LoginController 틀 제작
    틀제작

--- HR_SYSTEM/src/main/java/com/precursor/hrsystem/mvc/login/a01_controller/LoginController.java
+++ HR_SYSTEM/src/main/java/com/precursor/hrsystem/mvc/login/a01_controller/LoginController.java
@@ -1,5 +1,13 @@
 package com.precursor.hrsystem.mvc.login.a01_controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("/")
 public class LoginController {
 
 }
