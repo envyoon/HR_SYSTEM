0584037ea32aa01f9d83691debe2fa55ac3e5490
envyoon <krafftdj@daum.net>
2022-12-22 17:25:20 +0900

    VO 수정
    getter setter가 이름이 이상했음.
    그래서 소문자로 처리하였고,
    Generate toString().. 도 추가하였음

--- HR_SYSTEM/src/main/java/com/precursor/hrsystem/mvc/hrlist/a04_vo/HrListTest.java
+++ HR_SYSTEM/src/main/java/com/precursor/hrsystem/mvc/hrlist/a04_vo/HrListTest.java
@@ -3,119 +3,153 @@ package com.precursor.hrsystem.mvc.hrlist.a04_vo;
 import java.sql.Date;
 
 public class HrListTest {
-	private int IM_KEY;
-	private String IM_USER_ID;
-	private String IM_USER_NAME_KR;
-	private String IM_USER_NAME_EN;
-	private String IM_USER_RANK;
-	private String IM_DEPT;
-	private String IM_USER_MAIL;
-	private String IM_PHONE_NUM;
-	private Date IM_HIRE_DATE;
-	private Date IM_UPDATE;
-	private String IM_USER_PW;
-	private String IM_FLAG;
-	private String IM_STATUS;
-	private Date ERCO_YMD;
-	private Date RTRM_YMD;
-	private Date BIRYMD;
+	private int im_key;
+	private String im_user_id;
+	private String im_user_name_kr;
+	private String im_user_name_en;
+	private String im_user_rank;
+	private String im_dept;
+	private String im_user_mail;
+	private String im_phone_num;
+	private Date im_hire_date;
+	private Date im_update;
+	private String im_user_pw;
+	private String im_flag;
+	private String im_status;
+	private Date erco_ymd;
+	private Date rtrm_ymd;
+	private Date birymd;
 	
-	public int getIM_KEY() {
-		return IM_KEY;
+	public HrListTest(int im_key, String im_user_id, String im_user_name_kr, String im_user_name_en,
+			String im_user_rank, String im_dept, String im_user_mail, String im_phone_num, Date im_hire_date,
+			Date im_update, String im_user_pw, String im_flag, String im_status, Date erco_ymd, Date rtrm_ymd,
+			Date birymd) {
+		super();
+		this.im_key = im_key;
+		this.im_user_id = im_user_id;
+		this.im_user_name_kr = im_user_name_kr;
+		this.im_user_name_en = im_user_name_en;
+		this.im_user_rank = im_user_rank;
+		this.im_dept = im_dept;
+		this.im_user_mail = im_user_mail;
+		this.im_phone_num = im_phone_num;
+		this.im_hire_date = im_hire_date;
+		this.im_update = im_update;
+		this.im_user_pw = im_user_pw;
+		this.im_flag = im_flag;
+		this.im_status = im_status;
+		this.erco_ymd = erco_ymd;
+		this.rtrm_ymd = rtrm_ymd;
+		this.birymd = birymd;
 	}
-	public void setIM_KEY(int iM_KEY) {
-		IM_KEY = iM_KEY;
+	
+	public int getIm_key() {
+		return im_key;
 	}
-	public String getIM_USER_ID() {
-		return IM_USER_ID;
+	public String getIm_user_id() {
+		return im_user_id;
 	}
-	public void setIM_USER_ID(String iM_USER_ID) {
-		IM_USER_ID = iM_USER_ID;
+	public String getIm_user_name_kr() {
+		return im_user_name_kr;
 	}
-	public String getIM_USER_NAME_KR() {
-		return IM_USER_NAME_KR;
+	public String getIm_user_name_en() {
+		return im_user_name_en;
 	}
-	public void setIM_USER_NAME_KR(String iM_USER_NAME_KR) {
-		IM_USER_NAME_KR = iM_USER_NAME_KR;
+	public String getIm_user_rank() {
+		return im_user_rank;
 	}
-	public String getIM_USER_NAME_EN() {
-		return IM_USER_NAME_EN;
+	public String getIm_dept() {
+		return im_dept;
 	}
-	public void setIM_USER_NAME_EN(String iM_USER_NAME_EN) {
-		IM_USER_NAME_EN = iM_USER_NAME_EN;
+	public String getIm_user_mail() {
+		return im_user_mail;
 	}
-	public String getIM_USER_RANK() {
-		return IM_USER_RANK;
+	public String getIm_phone_num() {
+		return im_phone_num;
 	}
-	public void setIM_USER_RANK(String iM_USER_RANK) {
-		IM_USER_RANK = iM_USER_RANK;
+	public Date getIm_hire_date() {
+		return im_hire_date;
 	}
-	public String getIM_DEPT() {
-		return IM_DEPT;
+	public Date getIm_update() {
+		return im_update;
 	}
-	public void setIM_DEPT(String iM_DEPT) {
-		IM_DEPT = iM_DEPT;
+	public String getIm_user_pw() {
+		return im_user_pw;
 	}
-	public String getIM_USER_MAIL() {
-		return IM_USER_MAIL;
+	public String getIm_flag() {
+		return im_flag;
 	}
-	public void setIM_USER_MAIL(String iM_USER_MAIL) {
-		IM_USER_MAIL = iM_USER_MAIL;
+	public String getIm_status() {
+		return im_status;
 	}
-	public String getIM_PHONE_NUM() {
-		return IM_PHONE_NUM;
+	public Date getErco_ymd() {
+		return erco_ymd;
 	}
-	public void setIM_PHONE_NUM(String iM_PHONE_NUM) {
-		IM_PHONE_NUM = iM_PHONE_NUM;
+	public Date getRtrm_ymd() {
+		return rtrm_ymd;
 	}
-	public Date getIM_HIRE_DATE() {
-		return IM_HIRE_DATE;
+	public Date getBirymd() {
+		return birymd;
 	}
-	public void setIM_HIRE_DATE(Date iM_HIRE_DATE) {
-		IM_HIRE_DATE = iM_HIRE_DATE;
+	public void setIm_key(int im_key) {
+		this.im_key = im_key;
 	}
-	public Date getIM_UPDATE() {
-		return IM_UPDATE;
+	public void setIm_user_id(String im_user_id) {
+		this.im_user_id = im_user_id;
 	}
-	public void setIM_UPDATE(Date iM_UPDATE) {
-		IM_UPDATE = iM_UPDATE;
+	public void setIm_user_name_kr(String im_user_name_kr) {
+		this.im_user_name_kr = im_user_name_kr;
 	}
-	public String getIM_USER_PW() {
-		return IM_USER_PW;
+	public void setIm_user_name_en(String im_user_name_en) {
+		this.im_user_name_en = im_user_name_en;
 	}
-	public void setIM_USER_PW(String iM_USER_PW) {
-		IM_USER_PW = iM_USER_PW;
+	public void setIm_user_rank(String im_user_rank) {
+		this.im_user_rank = im_user_rank;
 	}
-	public String getIM_FLAG() {
-		return IM_FLAG;
+	public void setIm_dept(String im_dept) {
+		this.im_dept = im_dept;
 	}
-	public void setIM_FLAG(String iM_FLAG) {
-		IM_FLAG = iM_FLAG;
+	public void setIm_user_mail(String im_user_mail) {
+		this.im_user_mail = im_user_mail;
 	}
-	public String getIM_STATUS() {
-		return IM_STATUS;
+	public void setIm_phone_num(String im_phone_num) {
+		this.im_phone_num = im_phone_num;
 	}
-	public void setIM_STATUS(String iM_STATUS) {
-		IM_STATUS = iM_STATUS;
+	public void setIm_hire_date(Date im_hire_date) {
+		this.im_hire_date = im_hire_date;
 	}
-	public Date getERCO_YMD() {
-		return ERCO_YMD;
+	public void setIm_update(Date im_update) {
+		this.im_update = im_update;
 	}
-	public void setERCO_YMD(Date eRCO_YMD) {
-		ERCO_YMD = eRCO_YMD;
+	public void setIm_user_pw(String im_user_pw) {
+		this.im_user_pw = im_user_pw;
 	}
-	public Date getRTRM_YMD() {
-		return RTRM_YMD;
+	public void setIm_flag(String im_flag) {
+		this.im_flag = im_flag;
 	}
-	public void setRTRM_YMD(Date rTRM_YMD) {
-		RTRM_YMD = rTRM_YMD;
+	public void setIm_status(String im_status) {
+		this.im_status = im_status;
 	}
-	public Date getBIRYMD() {
-		return BIRYMD;
+	public void setErco_ymd(Date erco_ymd) {
+		this.erco_ymd = erco_ymd;
 	}
-	public void setBIRYMD(Date bIRYMD) {
-		BIRYMD = bIRYMD;
+	public void setRtrm_ymd(Date rtrm_ymd) {
+		this.rtrm_ymd = rtrm_ymd;
 	}
+	public void setBirymd(Date birymd) {
+		this.birymd = birymd;
+	}
+
+	@Override
+	public String toString() {
+		return "HrListTest [im_key=" + im_key + ", im_user_id=" + im_user_id + ", im_user_name_kr=" + im_user_name_kr
+				+ ", im_user_name_en=" + im_user_name_en + ", im_user_rank=" + im_user_rank + ", im_dept=" + im_dept
+				+ ", im_user_mail=" + im_user_mail + ", im_phone_num=" + im_phone_num + ", im_hire_date=" + im_hire_date
+				+ ", im_update=" + im_update + ", im_user_pw=" + im_user_pw + ", im_flag=" + im_flag + ", im_status="
+				+ im_status + ", erco_ymd=" + erco_ymd + ", rtrm_ymd=" + rtrm_ymd + ", birymd=" + birymd + "]";
+	}
+	
+	
 	
 	
 }
