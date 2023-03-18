6f879e32a13ccb4e06faa784abd73bc50d3ebf6f
envyoon <krafftdj@daum.net>
2023-03-18 11:18:11 +0900

    feat: HrListVO 제작 (실 원장)

--- HR_SYSTEM/src/main/java/com/precursor/hrsystem/mvc/a99_vo/HrList.java
+++ HR_SYSTEM/src/main/java/com/precursor/hrsystem/mvc/a99_vo/HrList.java
@@ -1,6 +1,39 @@
 package com.precursor.hrsystem.mvc.a99_vo;
 
-public class HrList {
+import java.sql.Date;
 
-	// HR 원장대로 제작하여야 함.
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HrList extends PagingVO{
+
+	private String usrUdfRetireStatus;
+	private String usrLogin;
+	private Date usrStartDate;
+	private Date usrEndDate;
+	private String usrLastName;
+	private String usrFirstName;
+	private String usrUdfKorname;
+	private String usrUdfEngname;
+	private String usrUdfDeptCenCode;
+	private String usrUdfDeptCenName;
+	private String usrUdfUserClassiName;
+	private String usrUdfUserClassiCode;
+	private String usrEmail;
+	private String usrUdfPosCode;
+	private String usrUdfAutopassPosName;
+	private String usrUdfAutopassPosCode;
+	private String usrUdfAutopassLocationCode;
+	private String usrUdfAutopassLocationName;
+	private String usrDescription;
+	private Date usrUpdateDate;
+	
 }
